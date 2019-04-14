60e397d330e589d89c3815e34a511b0d4e5c43d0
Cosnomi <cosnomi@cosnomi.com>
2019-04-14 17:29:19 +0900

    Update slack icon and username

--- fetch/src/slack_webhook.py
+++ fetch/src/slack_webhook.py
@@ -2,9 +2,15 @@ import requests
 import os
 import json
 
+USERNAME = "ConoHa 料金通知"
+
 
 def send_slack_message(month, amount):
-    payload = {"text": "{}月分の請求額は{}円です。".format(month, amount)}
+    payload = {
+        "username": USERNAME,
+        "icon_url": os.environ['SLACK_ICON_URL'],
+        "text": "{}月分の請求額は{}円です。".format(month, amount)
+    }
     requests.post(
         os.environ['SLACK_WEBHOOK_URL'],
         data=json.dumps(payload).encode("utf-8"))
