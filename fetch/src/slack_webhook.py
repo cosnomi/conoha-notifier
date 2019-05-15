c710fb0b5aa259227a04cd9fc825b0c95c6ec01b
Cosnomi <cosnomi@cosnomi.com>
2019-05-15 18:58:17 +0900

    Pass invoice data using Invoice type

--- fetch/src/slack_webhook.py
+++ fetch/src/slack_webhook.py
@@ -1,15 +1,21 @@
 import requests
 import os
 import json
+from invoice import Invoice
+from typing import List
 
 USERNAME = "ConoHa 料金通知"
 
 
-def send_slack_message(month, amount):
+def send_slack_message(month, invoices: List[Invoice]):
     payload = {
-        "username": USERNAME,
-        "icon_url": os.environ['SLACK_ICON_URL'],
-        "text": "{}月分の請求額は{}円です。".format(month, amount)
+        "username":
+            USERNAME,
+        "icon_url":
+            os.environ['SLACK_ICON_URL'],
+        "text":
+            "{}月分の請求額は{}円です。".format(month,
+                                     sum([i.bill_plus_tax for i in invoices]))
     }
     requests.post(
         os.environ['SLACK_WEBHOOK_URL'],
