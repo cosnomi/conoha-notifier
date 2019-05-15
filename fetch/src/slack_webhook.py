4057ef803e0617a4f8ed30b6adae035cbc7d1fcc
Cosnomi <cosnomi@cosnomi.com>
2019-05-16 01:34:31 +0900

    Send detailed invoice data via Slack

--- fetch/src/slack_webhook.py
+++ fetch/src/slack_webhook.py
@@ -8,14 +8,23 @@ USERNAME = "ConoHa 料金通知"
 
 
 def send_slack_message(month, invoices: List[Invoice]):
+    attachments = []
+    for invoice in invoices:
+        attachments.extend([{
+            "color": "#12b8d7",
+            "title": item.name,
+            "text": "{}円".format(item.quantity)
+        } for item in invoice.detailed])
     payload = {
         "username":
             USERNAME,
         "icon_url":
             os.environ['SLACK_ICON_URL'],
         "text":
-            "{}月分の請求額は{}円です。".format(month,
-                                     sum([i.bill_plus_tax for i in invoices]))
+            "{}月分の請求額は{}円です。".format(
+                month, sum([invoice.bill_plus_tax for invoice in invoices])),
+        "attachments":
+            attachments
     }
     requests.post(
         os.environ['SLACK_WEBHOOK_URL'],
